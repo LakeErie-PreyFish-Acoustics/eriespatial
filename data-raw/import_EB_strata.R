857a22e10106d7350c68f3d23c57c25fa1c91f19
J. Holden <jeremy.holden@ontario.ca>
2022-06-20 15:13:09 -0400

    applies code formating to existing code chunks

--- data-raw/import_EB_strata.R
+++ data-raw/import_EB_strata.R
@@ -7,9 +7,11 @@ base_ebstrata <- ggplot() +
   geom_sf(data = shape_ebstrata) +
   geom_sf_label(data = shape_ebstrata, aes(label = STRATUM), size = 2) +
   theme_classic() +
-  theme(legend.position = "None",
-        axis.title.x=element_blank(),
-        axis.title.y=element_blank())
+  theme(
+    legend.position = "None",
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank()
+  )
 
 usethis::use_data(base_ebstrata, overwrite = TRUE)
 usethis::use_data(shape_ebstrata, overwrite = TRUE)
@@ -18,9 +20,11 @@ shape_erieshore <- sf::st_read("data-raw/Lake_Erie_Shoreline")
 base_erieshore <- ggplot() +
   geom_sf(data = shape_erieshore) +
   theme_classic() +
-  theme(legend.position = "None",
-        axis.title.x=element_blank(),
-        axis.title.y=element_blank())
+  theme(
+    legend.position = "None",
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank()
+  )
 
 usethis::use_data(base_erieshore, overwrite = TRUE)
 usethis::use_data(shape_erieshore, overwrite = TRUE)
