7a9f2e4ffcc07dde4e02877fd907716847342002
J. Holden <jeremy.holden@ontario.ca>
2023-01-17 14:47:16 -0500

    tests 5mingrid_surv

---
+++ tests/testthat/test-5mingrid_surv.R
@@ -0,0 +1,15 @@
+test_that("5mincent_surv is ggplot", {
+  expect_s3_class(base_5mingrid_surv, "ggplot")
+})
+
+test_that("base_5mincent_surv plots", {
+  expect_no_error(base_5mingrid_surv)
+})
+
+test_that("5mincent_surv has a shapefile", {
+  expect_s3_class(shape_5mingrid_surv, "sf")
+})
+
+test_that("5mincent_surv shape file plots", {
+  expect_no_error(plot(shape_5mingrid_surv))
+})
